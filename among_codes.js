a73fe1509f1a6aefd0ce8a7368c07639ed602db9
koen <9023005@student.zadkine.nl>
2024-11-01 17:16:45 +0100

    update2

--- among_codes.js
+++ among_codes.js
@@ -20,17 +20,17 @@ among_codes = {
     {"func_name":"game", "code":"v9o0h9wjio", "info":"Hoe heet de meest verkochte game?", "ans":"Minecraft"},
     {"func_name":"auto", "code":"xnyojxey1f", "info":"Hoe heef de meest verkochte auto?<br><sub>Hint het is van Toyota en begint met cor.</sub>", "ans":"Corolla"},
     {"func_name":"populaire", "code":"9maw8pkcn9", "info":"Wie heeft Microsoft bedacht?", "ans":"Bill Gates"},
-    {"func_name":"knoop", "code":"fah2z9rblp", "info":"<br><img height='200px' src='img/platte-knoop.png'>", "ans":"Platte knoop"},
-    {"func_name":"meme", "code":"668ribynn0", "info":"Welk thee merk drink Kermit hier?<br><img height='200px' src='img/kermit.jpg'>", "ans":"Lipton"},
+    {"func_name":"knoop", "code":"fah2z9rblp", "info":"<br><img  src='img/platte-knoop.png'>", "ans":"Platte knoop"},
+    {"func_name":"meme", "code":"668ribynn0", "info":"Welk thee merk drink Kermit hier?<br><img  src='img/kermit.jpg'>", "ans":"Lipton"},
     {"func_name":"geschiedenis", "code":"2v8slvsvj5", "info":"Welk beroep wilde adolf hitler eerst", "ans":"schilder"}
   ],
   "r2":[
     {"func_name":"rekensom", "code":"e07no1w2kh", "info":"(25+75)×4−90", "ans":"310"},
     {"func_name":"scouting", "code":"jmwoxbh1ov", "info":"Hoeveel jaar bestaat scouting Hoogvliet?", "ans":"70"},
-    {"func_name":"game", "code":"oxfjtva1dz", "info":"Welke pikachu is correct?<br>Links of rechts?<br><img height='200px' src='img/pokemon.jpg'>", "ans":"links", "choose":true},
-    {"func_name":"auto", "code":"focx7go6gg", "info":"Welke is het correcte logo?<br>Links of rechts?<br><img height='200px' src='img/bmw.jpg'>", "ans":"links", "choose":true},
-    {"func_name":"populaire", "code":"dj6ljqbf3u", "info":"Wie is dit?<br><img height='200px' src='img/jochem.jpg'>", "ans":"Jochem Myjer"},
-    {"func_name":"knoop", "code":"bl4dx072cz", "info":"<br><img height='200px' src='img/Slippendeschootsteek.svg.png'>", "ans":"schootsteek"},
+    {"func_name":"game", "code":"oxfjtva1dz", "info":"Welke pikachu is correct?<br>Links of rechts?<br><img  src='img/pokemon.jpg'>", "ans":"links", "choose":true},
+    {"func_name":"auto", "code":"focx7go6gg", "info":"Welke is het correcte logo?<br>Links of rechts?<br><img  src='img/bmw.jpg'>", "ans":"links", "choose":true},
+    {"func_name":"populaire", "code":"dj6ljqbf3u", "info":"Wie is dit?<br><img  src='img/jochem.jpg'>", "ans":"Jochem Myjer"},
+    {"func_name":"knoop", "code":"bl4dx072cz", "info":"<br><img  src='img/Slippendeschootsteek.svg.png'>", "ans":"schootsteek"},
     {"func_name":"meme", "code":"nvk8f73q0l", "info":"Waarom?", "ans":"daarom"},
     {"func_name":"geschiedenis", "code":"ildsi3aey0", "info":"Op welke datum is bevrijdingsdag?", "ans":"5 mei"}
   ],
@@ -38,10 +38,10 @@ among_codes = {
     {"func_name":"rekensom", "code":"fgd76egd05", "info":"(20+30)×2−15=", "ans":"85"},
     {"func_name":"scouting", "code":"s4lcplfm36", "info":"Waar staat de LWS voor?", "ans":"LandelijkeScoutingwedstrijden"},
     {"func_name":"game", "code":"1sf9howk0p", "info":"Wat is de game naam van de maker van Minecraft?", "ans":"Notch"},
-    {"func_name":"auto", "code":"xg1oe9t8pd", "info":"Welke logo moet deze positie voorstellen<br><img height='200px' src='img/foto_ofzo.jpg'>", "ans":"volkswagen"},
-    {"func_name":"populaire", "code":"pkltnvviqc", "info":"Wie is dit?<br><img height='200px' src='img/arjenlubach.jpg'>", "ans":"arjenlubach.jpg"},
-    {"func_name":"knoop", "code":"ddow24fqkr", "info":"<br><img height='200px' src='img/mastworp.png'>", "ans":"mastworp"},
-    {"func_name":"meme", "code":"7zuigm0f78", "info":"Van welke show is deze meme?<br><img height='200px' src='img/office_meme.png'>", "ans":"The office"},
+    {"func_name":"auto", "code":"xg1oe9t8pd", "info":"Welke logo moet deze positie voorstellen<br><img  src='img/foto_ofzo.jpg'>", "ans":"volkswagen"},
+    {"func_name":"populaire", "code":"pkltnvviqc", "info":"Wie is dit?<br><img  src='img/arjenlubach.jpg'>", "ans":"arjenlubach.jpg"},
+    {"func_name":"knoop", "code":"ddow24fqkr", "info":"<br><img  src='img/mastworp.png'>", "ans":"mastworp"},
+    {"func_name":"meme", "code":"7zuigm0f78", "info":"Van welke show is deze meme?<br><img  src='img/office_meme.png'>", "ans":"The office"},
     {"func_name":"geschiedenis", "code":"vmm6z8sbtz", "info":"Wanneer is pakjesavond?", "ans":"5 december"}
   ],
   "r4":[
@@ -49,9 +49,9 @@ among_codes = {
     {"func_name":"scouting", "code":"zanflp5ffu", "info":"Waar staat de rWS voor?", "ans":"RegionaleScoutingWedstrijden"},
     {"func_name":"game", "code":"k65d2xblhg", "info":"Welke munteenheid gebruikt Fortnite?", "ans":"vbucks"},
     {"func_name":"auto", "code":"rn6663p8i4", "info":"Welke autofabrikant maakt het model 'Civic'?", "ans":"Honda"},
-    {"func_name":"populaire", "code":"j1ku8nvc21", "info":"Wie is dit?<br><img height='200px' src='img/bob.jpg'>", "ans":"Bob Marley"},
-    {"func_name":"knoop", "code":"b9czn215v2", "info":"<br><img height='200px' src='img/paalsteek.jpg'>", "ans":"paalsteek"},
-    {"func_name":"meme", "code":"83yanog06s", "info":"Hoe heet de artiest in deze meme?<br><img height='200px' src='img/Hotline-Bling.jpg'>", "ans":"Drake"},
+    {"func_name":"populaire", "code":"j1ku8nvc21", "info":"Wie is dit?<br><img  src='img/bob.jpg'>", "ans":"Bob Marley"},
+    {"func_name":"knoop", "code":"b9czn215v2", "info":"<br><img  src='img/paalsteek.jpg'>", "ans":"paalsteek"},
+    {"func_name":"meme", "code":"83yanog06s", "info":"Hoe heet de artiest in deze meme?<br><img  src='img/Hotline-Bling.jpg'>", "ans":"Drake"},
     {"func_name":"geschiedenis", "code":"pov45mocui", "info":"Uit welk land komen tulpen.", "ans":"Turkije"}
   ],
   "r5":[
@@ -60,8 +60,8 @@ among_codes = {
     {"func_name":"game", "code":"a1a97sb6rh", "info":"Wat is de naam van de stad waar GTA 5 afspeeld?", "ans":"Los Santos"},
     {"func_name":"auto", "code":"xvz1ogcqvd", "info":"Welke autofabrikant produceert het model 'Impreza'?", "ans":"Subaru"},
     {"func_name":"populaire", "code":"jmmlfvvp5d", "info":"wie heeft romeo en julia geschreven", "ans":"William Shakespeare"},
-    {"func_name":"knoop", "code":"ua298qf19r", "info":"<br><img height='200px' src='img/achtknoop.jpg'>", "ans":"Achtknoop"},
-    {"func_name":"meme", "code":"l7njggo8aq", "info":"<img height='200px' src='img/dude.jpg'>", "ans":"rechts", "choose":true},
+    {"func_name":"knoop", "code":"ua298qf19r", "info":"<br><img  src='img/achtknoop.jpg'>", "ans":"Achtknoop"},
+    {"func_name":"meme", "code":"l7njggo8aq", "info":"<img  src='img/dude.jpg'>", "ans":"rechts", "choose":true},
     {"func_name":"geschiedenis", "code":"dirafgfxkd", "info":"Wat voor dier is sven<br><sub>van Frozen.</sub>", "ans":"rendier"}
   ],
   "r6":[
@@ -70,17 +70,17 @@ among_codes = {
     {"func_name":"game", "code":"qs1oh7s6k6", "info":"Wat is het best verkochte spel voor de Nintendo Switch?", "ans":"Mario Kart"},
     {"func_name":"auto", "code":"99snf4hium", "info":"Wat voor soort auto is optimus prime?", "ans":"vrachtwagen"},
     {"func_name":"populaire", "code":"s1vx5qayn5", "info":"Welke vorm van autisme heeft Elon Musk", "ans":"Asperger"},
-    {"func_name":"knoop", "code":"fpgmdd4zjg", "info":"<br><img height='200px' src='img/Halvesteek.svg.png'>", "ans":"Halve steek"},
+    {"func_name":"knoop", "code":"fpgmdd4zjg", "info":"<br><img  src='img/Halvesteek.svg.png'>", "ans":"Halve steek"},
     {"func_name":"meme", "code":"2ygnn6zee9", "info":"Ik ga omhoog en omlaag, maar ik beweeg nooit. Wat ben ik?", "ans":"trap"},
     {"func_name":"geschiedenis", "code":"07au5wvovc", "info":"Wat was de originele kleur van de jurk assepoester?", "ans":"zilver"}
   ],
   "r7":[
     {"func_name":"rekensom", "code":"yy1ll1vd3x", "info":"(60÷3)+45−20=", "ans":"45"},
-    {"func_name":"scouting", "code":"eej9iptvpe", "info":"", "ans":""},
+    {"func_name":"scouting", "code":"eej9iptvpe", "info":"Welke kleur is Cambray?<br><img src='img/kleur6.png'>", "ans":"links", "choose":true},
     {"func_name":"game", "code":"4jx4fjg53g", "info":"Welke munteenheid gebruikt Roblox?", "ans":"robux"},
-    {"func_name":"auto", "code":"4kkbp1dpgn", "info":"", "ans":""},
+    {"func_name":"auto", "code":"4kkbp1dpgn", "info":"Wat voor merk auto is dit?<img  src='img/truck.jpg'>", "ans":"Tesla"},
     {"func_name":"populaire", "code":"t560o0nqun", "info":"Hoe heet de knuffelbeer van Mr Bean?", "ans":"Teddy"},
-    {"func_name":"knoop", "code":"j7sl9n8ymc", "info":"", "ans":""},
+    {"func_name":"knoop", "code":"j7sl9n8ymc", "info":"Welke kleur is Amarant?<br><img src='img/kleur5.png'>", "ans":"rechts", "choose":true},
     {"func_name":"meme", "code":"woa6vjfv2h", "info":"Ik heb geen ogen, maar ik kan toch zien. Wat ben ik?", "ans":"camera"},
     {"func_name":"geschiedenis", "code":"d66iu17dvi", "info":"Hoe heten de landen België Nederland en Luxemburg bij elkaar?", "ans":"Benelux"}
   ],
@@ -116,6 +116,6 @@ among_codes = {
   ],
   "r11":[
     {"func_name":"random", "code":"temp", "info":"dit is een vraag", "ans":"antwoord"},
-    {"func_name":"random", "code":"temp2", "info":"afbeelding?<br><img height='200px' src='img/amogus2.png'>", "ans":"links", "choose":true}
+    {"func_name":"random", "code":"temp2", "info":"afbeelding?<br><img  src='img/amogus2.png'>", "ans":"links", "choose":true}
   ]
   }
